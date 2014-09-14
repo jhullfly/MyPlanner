a14454a92e2f545a466fde4d6564ffe0b30a2fb5
jhullfly <jessehull@gmail.com>
2014-09-13 23:11:02 -0700

    Sorting on main table

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,15 +5,25 @@ myPlannerAppControllers.factory('ParseObjectFactory', function() {
 		buildFriendRelation : function() {
 			return Parse.Object.extend({
 				className: "FriendRelation",
-				attrs: ['friendName', 'inPhoto', 'photoTaken', 'tookPhoto'],
+				attrs: ['friendName', 'inPhoto', 'photoTaken', 'tookPhoto']
 			});
 		}
 	}
 });
 
 myPlannerAppControllers.controller('ListCtrl', ['$scope', 'ParseObjectFactory', function ($scope, ParseObjectFactory) {
-	var query = new Parse.Query(ParseObjectFactory.buildFriendRelation());
-	query.descending("inPhoto");
+    var FriendRelation = ParseObjectFactory.buildFriendRelation();
+    $scope.columns = ['friendName', 'inPhoto', 'photoTaken', 'tookPhoto'];
+    $scope.sort = {
+        column: 1,
+        descending: true
+    };
+	var query = new Parse.Query(FriendRelation);
+    if ($scope.sort.descending) {
+        query.descending($scope.columns[$scope.sort.column]);
+    } else {
+        query.ascending($scope.columns[$scope.sort.column]);
+    }
 	query.limit(500);
 	query.find({
 		success:function(results) {
@@ -23,4 +33,22 @@ myPlannerAppControllers.controller('ListCtrl', ['$scope', 'ParseObjectFactory', 
 			console.error("Error: " + error.code + " " + error.message);
 		}		
 	});
+
+    $scope.selectedCls = function(column) {
+        return column == $scope.sort.column && 'sort-' + $scope.sort.descending;
+    };
+
+    $scope.changeSorting = function(column) {
+        var sort = $scope.sort;
+        if (sort.column == column) {
+            sort.descending = !sort.descending;
+        } else {
+            sort.column = column;
+            sort.descending = true;
+        }
+    };
+
+    $scope.orderByFunc = function(relation) {
+        return relation.get($scope.columns[$scope.sort.column]);
+    }
 }]);
